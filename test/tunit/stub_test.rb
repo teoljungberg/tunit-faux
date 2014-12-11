de0f104bb628f0c5b4d1337aa09a7806f47ad464
Teo Ljungberg <teo@teoljungberg.com>
2014-12-11 19:00:36 +0100

    Refactor StubTest
    - move `test_klass` scoping into setup-method

--- test/tunit/stub_test.rb
+++ test/tunit/stub_test.rb
@@ -3,15 +3,18 @@ remove_minitest_stub_definition!
 require "tunit/stub"
 
 class StubTest < Minitest::Test
+  def setup
+    @test_klass = self
+  end
+  attr_reader :test_klass
+
   def test_stub_only_stubs_already_defined_methods
     def self.foo
       42
     end
 
-    tc = self
-
     self.stub :foo, 1337 do
-      tc.assert_equal 1337, self.foo
+      test_klass.assert_equal 1337, self.foo
     end
   end
 
@@ -20,10 +23,8 @@ class StubTest < Minitest::Test
       42
     end
 
-    tc = self
-
     self.stub :foo, 1337 do |stub|
-      tc.assert_equal 1337, stub.foo
+      test_klass.assert_equal 1337, stub.foo
     end
   end
 
@@ -32,18 +33,14 @@ class StubTest < Minitest::Test
       1
     end
 
-    tc = self
-
     self.stub :foo, lambda {|n| n * 2 } do
-      tc.assert_equal 42, self.foo(21)
+      test_klass.assert_equal 42, self.foo(21)
     end
   end
 
   def test_stubs_private_and_public_method
-    tc = self
-
-    tc.stub :private_method, "stubbed" do
-      tc.assert_equal "stubbed", tc.private_method
+    test_klass.stub :private_method, "stubbed" do
+      test_klass.assert_equal "stubbed", test_klass.private_method
     end
   end
 
@@ -62,11 +59,9 @@ class StubTest < Minitest::Test
       42
     end
 
-    tc = self
-
     self.stub :foo, 1337 do
-      tc.assert_includes self.methods.map(&:to_s), "__temporary_stub_foo__"
-      tc.assert_equal 42, self.send(:__temporary_stub_foo__)
+      test_klass.assert_includes self.methods.map(&:to_s), "__temporary_stub_foo__"
+      test_klass.assert_equal 42, self.send(:__temporary_stub_foo__)
     end
 
     assert_equal 42, self.foo

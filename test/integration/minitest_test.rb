56a9c5b7e67f078cee52a4412fd9fc6263ea12f3
Teo Ljungberg <teo@teoljungberg.com>
2016-11-19 17:47:46 +0100

    Update assertion test cases to cover all variations

--- test/integration/minitest_test.rb
+++ test/integration/minitest_test.rb
@@ -130,49 +130,145 @@ module Integration
           def test_assertion
             greeter = spy
 
+            greeter.greet
+
+            assert_received greeter, :greet
+          end
+
+          def test_assertion_with_arguments
+            greeter = spy
+
             greeter.greet("world")
 
             assert_received greeter, :greet, with: "world"
           end
 
+          def test_assertion_with_times
+            greeter = spy
+
+            greeter.greet("world")
+            greeter.greet("world")
+
+            assert_received greeter, :greet, times: 2, with: "world"
+          end
+
           def test_refution
             greeter = spy
 
-            greeter.greet("bye")
+            refute_received greeter, :greet
+          end
+
+          def test_refution_with_arguments
+            greeter = spy
+
+            greeter.greet("hello")
 
             refute_received greeter, :greet, with: "world"
+          end
+
+          def test_refution_with_times
+            greeter = spy
+
+            greeter.greet("world")
+            greeter.greet("world")
+
+            refute_received greeter, :greet, times: 1, with: "world"
           end
         end
         test_case_assertion = Minitest.run_one_method(
           test_klass,
           :test_assertion,
         )
+        test_case_assertion_with_arguments = Minitest.run_one_method(
+          test_klass,
+          :test_assertion_with_arguments,
+        )
+        test_case_assertion_with_times = Minitest.run_one_method(
+          test_klass,
+          :test_assertion_with_times,
+        )
         test_case_refution = Minitest.run_one_method(
           test_klass,
           :test_refution,
         )
+        test_case_refution_with_arguments = Minitest.run_one_method(
+          test_klass,
+          :test_refution_with_arguments,
+        )
+        test_case_refution_with_times = Minitest.run_one_method(
+          test_klass,
+          :test_refution_with_times,
+        )
 
         assert test_case_assertion.passed?
+        assert test_case_assertion_with_arguments.passed?
+        assert test_case_assertion_with_times.passed?
         assert test_case_refution.passed?
+        assert test_case_refution_with_arguments.passed?
+        assert test_case_refution_with_times.passed?
       end
 
       def test_integration_assertions_failure
         test_klass = Class.new(Minitest::Test) do
-          def test_assertions
+          def test_refution
             greeter = spy
 
-            greeter.greet("bye")
+            assert_received greeter, :greet
+          end
+
+          def test_refution_with_arguments
+            greeter = spy
+
+            greeter.greet("hello")
 
             assert_received greeter, :greet, with: "world"
           end
+
+          def test_refution_with_times
+            greeter = spy
+
+            greeter.greet("world")
+            greeter.greet("world")
+
+            assert_received greeter, :greet, times: 1, with: "world"
+          end
         end
-        test_case = Minitest.run_one_method(test_klass, :test_assertions)
+        test_case_refution = Minitest.run_one_method(
+          test_klass,
+          :test_refution,
+        )
+        test_case_refution_with_arguments = Minitest.run_one_method(
+          test_klass,
+          :test_refution_with_arguments,
+        )
+        test_case_refution_with_times = Minitest.run_one_method(
+          test_klass,
+          :test_refution_with_times,
+        )
 
         exp_message = <<~EOS
-        Expected Spy(anonymous)#greet["world"] to have been called, was called with ["bye"]
+          Expected Spy(anonymous)#greet[] to have been called
         EOS
-        refute test_case.passed?
-        assert_equal exp_message.strip, test_case.failure.error.to_s
+        refute test_case_refution.passed?
+        assert_equal exp_message.strip, test_case_refution.failure.error.to_s
+
+        exp_message = <<~EOS
+          Expected Spy(anonymous)#greet["world"] to have been called, was called with ["hello"]
+        EOS
+        refute test_case_refution_with_arguments.passed?
+        assert_equal(
+          exp_message.strip,
+          test_case_refution_with_arguments.failure.error.to_s,
+        )
+
+        exp_message = <<~EOS
+          Expected Spy(anonymous)#greet["world"] to have been called 1 time, was called 2 times
+        EOS
+        refute test_case_refution_with_times.passed?
+        assert_equal(
+          exp_message.strip,
+          test_case_refution_with_times.failure.error.to_s,
+        )
       end
     end
   end
